a6a20523870aa98aad0ea2add850709a54196811
Justin Lawen <Justin@LesserHydra.com>
2018-01-31 14:14:02 -0600

    Add damager paths for effect clouds and tnt

--- src/main/java/com/lesserhydra/nemesis/bukkitutil/EntityUtils.java
+++ src/main/java/com/lesserhydra/nemesis/bukkitutil/EntityUtils.java
@@ -1,8 +1,10 @@
 package com.lesserhydra.nemesis.bukkitutil;
 
+import org.bukkit.entity.AreaEffectCloud;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.Projectile;
+import org.bukkit.entity.TNTPrimed;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.projectiles.ProjectileSource;
 
@@ -10,7 +12,7 @@ public class EntityUtils {
 	
 	/**
 	 * Gets an attacking LivingEntity from the damager Entity. Looks for the
-	 * projectile source if damager is a projectile.
+	 * source if damager is a projectile, effect cloud, or tnt.
 	 * @param damager Damager from EntityDamagedByEntityEvent
 	 * @return The living entity that caused the damage event, or null
 	 */
@@ -21,6 +23,16 @@ public class EntityUtils {
 		//Damager is projectile
 		if (damager instanceof Projectile) {
 			ProjectileSource source = ((Projectile) damager).getShooter();
+			if (source instanceof LivingEntity) return (LivingEntity) source;
+		}
+		//Damager is effect cloud
+		else if (damager instanceof AreaEffectCloud) {
+			ProjectileSource source = ((AreaEffectCloud) damager).getSource();
+			if (source instanceof LivingEntity) return (LivingEntity) source;
+		}
+		//Damager is tnt
+		else if (damager instanceof TNTPrimed) {
+			Entity source = ((TNTPrimed) damager).getSource();
 			if (source instanceof LivingEntity) return (LivingEntity) source;
 		}
 		
